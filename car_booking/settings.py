2d494553bce3bc051b666a5d9ef849aad925afb7
ditya-glitch <ditya4592@gmail.com>
2026-08-12 17:57:07 +0530

    Fix allowed hosts for Render

--- car_booking/settings.py
+++ car_booking/settings.py
@@ -25,7 +25,11 @@ SECRET_KEY = 'django-insecure-wh4e-+c19d$coi6nv*onbvdsbsn$1mo4x-36*_$8qd2xh8=trl
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    "car-booking-portal-3.onrender.com",
+    "localhost",
+    "127.0.0.1",
+]
 
 
 # Application definition
